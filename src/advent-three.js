a19f3295f1543dc5eb17b5853087adee4186f026
Dave Hitchings <dhitchings@commonbond.co>
2017-12-05 11:23:10 -0500

    part two

--- src/advent-three.js
+++ src/advent-three.js
@@ -43,8 +43,65 @@ export function adventThreePartOne(input) {
   return Math.abs(movement.horizontal) + Math.abs(movement.vertical);
 }
 
-export function adventThreePartTwo() {
-  return 0;
+export function adventThreePartTwo(input) {
+  let ringWidth = 1;
+  const arr = [];
+
+  while (input > ringWidth ** 2) {
+    ringWidth += 2;
+  }
+
+  for (let i = 0; i < ringWidth; i += 1) {
+    const subArr = [];
+    for (let j = 0; j < ringWidth; j += 1) {
+      subArr.push(0);
+    }
+    arr.push(subArr);
+  }
+
+  const center = Math.floor(ringWidth / 2);
+  let currX = center;
+  let currY = center;
+
+  ringWidth = 1;
+  arr[currY][currX] = 1;
+
+  let lastVal = 1;
+  let position = 1;
+  while (input >= lastVal) {
+    const edge = ringWidth - 1;
+    const first = (ringWidth - 2) ** 2;
+
+    if (first + edge > position) {
+      currY -= 1;
+    } else if (first + (edge * 2) > position) {
+      currX -= 1;
+    } else if (first + (edge * 3) > position) {
+      currY += 1;
+    } else {
+      currX += 1;
+    }
+
+    if (position === ringWidth ** 2) {
+      ringWidth += 2;
+    }
+
+    lastVal =
+      arr[currY - 1][currX] +
+      arr[currY - 1][currX + 1] +
+      arr[currY][currX + 1] +
+      arr[currY + 1][currX + 1] +
+      arr[currY + 1][currX] +
+      arr[currY + 1][currX - 1] +
+      arr[currY][currX - 1] +
+      arr[currY - 1][currX - 1];
+
+    arr[currY][currX] = lastVal;
+
+    position += 1;
+  }
+
+  return lastVal;
 }
 
 console.log('Advent Three:');
@@ -58,5 +115,8 @@ console.log('Answer:', adventThreePartOne(289326));
 
 console.log('Part Two:');
 console.log('Tests:');
-test(adventThreePartTwo(), 0, '');
-console.log('Answer:', adventThreePartTwo());
+test(adventThreePartTwo(27), 54, '27');
+test(adventThreePartTwo(142), 147, '142');
+test(adventThreePartTwo(143), 147, '143');
+test(adventThreePartTwo(748), 806, '142');
+console.log('Answer:', adventThreePartTwo(289326));
